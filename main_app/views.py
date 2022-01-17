4b4970e79949bec7b350124a3526a2d7c83e81b4
Jazmin-Ewers <jazminewers11@gmail.com>
2022-01-17 13:27:00 -0500

    Add Skill Form Functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+
+from main_app.models import *
 
 # Create your views here.
 def home(request):
@@ -24,3 +27,12 @@ def signup(request):
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
+
+class SkillCreate(CreateView):
+  model = Skill
+  fields = '__all__'
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+  
