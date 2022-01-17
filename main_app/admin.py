8997002580b3e8733c913a90b51a6c153c62c0f2
Jazmin-Ewers <jazminewers11@gmail.com>
2022-01-17 15:03:57 -0500

    Update Models so that TYPES showcases value and register models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Contact, Skill, Experience, Education, Projects, Resume
+
 # Register your models here.
+admin.site.register(Contact)
+admin.site.register(Skill)
+admin.site.register(Experience)
+admin.site.register(Education)
+admin.site.register(Projects)
+admin.site.register(Resume)
+
+
