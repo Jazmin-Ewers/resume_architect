a3e76dd93d9353aa429f3099ee5b51de2f4148d0
Lauren N Massey <laurennmassey@Laurens-MacBook-Pro.local>
2022-01-18 10:44:28 -0800

    education form and index pages function

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
-from .models import Resume
+from .models import Resume, Education
 
 class ResumeForm(ModelForm):
-    class Meta:
-        model = Resume
-        fields = ['skills']
+  class Meta:
+    model = Resume
+    fields = ['skills']
+
+class EducationForm(ModelForm):
+  class Meta:
+    model = Education 
+    fields = '__all__'       
