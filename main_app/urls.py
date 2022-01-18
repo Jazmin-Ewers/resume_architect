14dc900e0007b3c97591cce790b19f431587bc6b
Megan Ratto <meganratto@Megans-MacBook-Air.local>
2022-01-18 11:25:29 -0800

    Add Skill update functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
   # Skills 
   path('skills/create/', views.SkillCreate.as_view(), name='skills_create'),
   path('skills/', views.skills_index, name='skills_index'),
+  path('skills/<int:pk>/update/', views.SkillUpdate.as_view(), name='skills_update'),
   #Resume paths
   path('resumes/create/', views.ResumeCreate.as_view(), name='resumes_create'),
   path('resumes/<int:resume_id>/', views.resumes_detail, name='resumes_detail'),
@@ -14,5 +15,8 @@ urlpatterns = [
   #Education
   path('educations/', views.educations_index, name='educations_index'),
   path('educations/create/', views.EducationCreate.as_view(), name='educations_create'),
+
+  # Contact Paths
+  path('contacts/', views.contacts_index, name='contacts_index'),
 
 ]
