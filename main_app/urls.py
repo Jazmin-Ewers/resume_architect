2878a0db54a621e1d9df758e4bf931532abbaeb6
Lauren N Massey <laurennmassey@Laurens-MacBook-Pro.local>
2022-01-19 13:16:18 -0800

    printing to html works

--- main_app/urls.py
+++ main_app/urls.py
@@ -18,7 +18,8 @@ urlpatterns = [
   path('resumes/create/', views.ResumeCreate.as_view(), name='resumes_create'),
   path('resumes/<int:resume_id>/', views.resumes_detail, name='resumes_detail'),
   path('resumes/<int:resume_id>/assoc_skill/<int:skill_id>/', views.assoc_skill, name='assoc_skill'),
-  
+  path('resumes/<int:resume_id>/print', views.resumes_print, name='resumes_print'),
+
   #Education
 
   path('educations/', views.educations_index, name='educations_index'),
